c0369a4f0023d40b17bfbaab3f1ec85f9f706bca
Jade_Rabbit <stnislav_dantes@mail.ru>
2026-03-07 15:10:59 +0500

    start1

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Возвращает маску номера карты в виде XXXX XX** **** XXXX"""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Возвращает маску номера счета в виде **ХХХХ"""
+    return "**" + account_number[-4:]
+
+
